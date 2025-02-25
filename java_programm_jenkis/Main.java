55c5f9d290ffa30468ee081625fc8080277a7e39
YashwantKhillare <khillareyash95@gmail.com>
2025-02-25 18:07:30 +0530

    Initial commit

---
+++ java_programm_jenkis/Main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("this is a java programm..!");
+        System.out.println("output from java programm..!");
+        System.out.println(new Date());
+    }
+
+}
